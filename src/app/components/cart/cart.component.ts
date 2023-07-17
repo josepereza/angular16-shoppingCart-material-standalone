ccd14e118769f7a4aec2e12ffb04ece300a335e3
josepereza <josepereza66@gmail.com>
2023-07-17 15:52:01 +0200

    first commit3

--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 import { CartService } from 'src/app/services/cart.service';
 import { Product } from 'src/app/interfaces/product';
 import {MatListModule} from '@angular/material/list';
+import {MatIconModule} from '@angular/material/icon';
+
 import { TotalComponent } from '../total/total.component';
 
 
 @Component({
   selector: 'app-cart',
   standalone: true,
-  imports: [CommonModule, MatListModule, TotalComponent],
+  imports: [CommonModule, MatListModule, TotalComponent,MatIconModule],
   templateUrl: './cart.component.html',
   styleUrls: ['./cart.component.css']
 })
